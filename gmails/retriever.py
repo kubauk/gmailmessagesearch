492882240308d68ac46675cd1723b14e681e9062
Jakub Jamro <kuba.jamro@gmail.com>
2021-05-04 23:16:18 +0100

    Add testing around time zones
    The GMail API expects dates and timestamps to be in PST.
    
    For time zone aware datetimes we just convert to PST.
    For time zone naive datetimes we need to specify a local time zone
    For dates there is never any timezone information so a local time zone also needs to be specified.

--- gmails/retriever.py
+++ gmails/retriever.py
@@ -1,33 +1,40 @@
 import base64
 import email
 import os
-from datetime import timedelta
+from datetime import date, datetime, time, timedelta
 from threading import Event
+from typing import Union
 
 import httplib2
-import oauth2client
-import pytz
 from googleapiclient import discovery
 from googleapiclient.http import BatchHttpRequest
 from oauth2client import client
 from oauth2client import file
 from oauth2client import tools
+from pytz import tzinfo, timezone
 
 _SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 _CLIENT_SECRET_FILE = 'client_secret.json'
-_US_PACIFIC_TZ = pytz.timezone('US/Pacific')
+_US_PACIFIC_TZ = timezone('US/Pacific')
 
 
-def as_us_pacific(date):
-    return date.astimezone(_US_PACIFIC_TZ)
+def as_timestamp(d: datetime) -> int:
+    return int(d.timestamp())
 
 
-def day_after(date):
-    return date + timedelta(days=1)
+def as_us_pacific(d: Union[date, datetime], local_time_zone: tzinfo = None) -> datetime:
+    if type(d) == date or not d.tzinfo:
+        if not local_time_zone:
+            raise ValueError("A local time zone must be specified if you are using dates or timezone naive datetimes")
+        else:
+            if type(d) == date:
+                d = datetime.combine(d, time())
+            d = local_time_zone.localize(d)
+    return d.astimezone(_US_PACIFIC_TZ)
 
 
-def as_query_date(date):
-    return date.strftime("%Y/%m/%d")
+def day_after(d: datetime) -> datetime:
+    return d + timedelta(days=1)
 
 
 def decode_message(result):
@@ -51,6 +58,7 @@ def _fake_arg_parser():
         noauth_local_webserver = False
         auth_host_port = [8080, 8090]
         logging_level = 'ERROR'
+
     return FakeArgParser()
 
 
@@ -64,12 +72,15 @@ class Retriever(object):
         self._email_address = email_address
         self._secrets_directory = secrets_directory
 
-    def get_messages_for_date(self, search_query, message_date):
-        return self._retrieve_messages(self._list_messages_for_day(search_query, message_date))
-
-    def get_messages_for_date_range(self, search_query, after_date, before_date):
+    def get_messages_for_date(self, search_query: str, message_date: Union[date, datetime],
+                              local_time_zone: tzinfo = None):
         return self._retrieve_messages(
-            self._list_messages_for_days(search_query, after_date, before_date))
+            self._list_messages_for_days(search_query, message_date, day_after(message_date), local_time_zone))
+
+    def get_messages_for_date_range(self, search_query: str, after_date: Union[date, datetime],
+                                    before_date: Union[date, datetime], local_time_zone: tzinfo = None):
+        return self._retrieve_messages(
+            self._list_messages_for_days(search_query, after_date, before_date, local_time_zone))
 
     def _get_service(self):
         if self._current_service is None:
@@ -77,18 +88,16 @@ class Retriever(object):
         return self._current_service
 
     def _get_credentials(self):
-        store = oauth2client.file.Storage(os.path.join(self._secrets_directory, "credentials.json"))
+        store = file.Storage(os.path.join(self._secrets_directory, "credentials.json"))
         flow = client.flow_from_clientsecrets(os.path.join(self._secrets_directory, _CLIENT_SECRET_FILE), _SCOPES)
         flow.user_agent = self._application_name
         return tools.run_flow(flow, store, self._args)
 
-    def _list_messages_for_day(self, search_query, date):
-        return self._list_messages_for_days(search_query, date, day_after(date))
-
-    def _list_messages_for_days(self, search_query, after, before):
-        query = '%s after:%s before:%s' % (search_query,
-                                           as_query_date(as_us_pacific(after)),
-                                           as_query_date(as_us_pacific(before)))
+    def _list_messages_for_days(self, search_query: str, after: Union[date, datetime], before: Union[date, datetime],
+                                local_time_zone: tzinfo = None):
+        query = '{} after:{} before:{}'.format(search_query,
+                                               as_timestamp(as_us_pacific(after, local_time_zone)),
+                                               as_timestamp(as_us_pacific(before, local_time_zone)))
         service = self._get_service()
         result = service.users().messages().list(userId=self._email_address, q=query).execute()
         message_ids = result.get('messages', [])
